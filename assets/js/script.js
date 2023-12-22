9c399a0744a055b2c09b4ad00282b69b962f2991
Marius Du Plooy <woestyn101@gmail.com>
2023-12-22 11:13:29 -0500

    console.log ingrediensts & instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 console.log("nut 02");
 
+var recipeSelect = document.getElementById("ChooseRecipe");
+var showIngredientList = document.getElementById("showIngredientList");
+var searchBtn = document.getElementById("searchBtn");
+var showIt = document.getElementById("showIt");
+var errorRecipe = document.getElementById("err-recipe");
+var recipeImage = document.getElementById("recipeImage");
+var showInstructiontionList = document.getElementById("showInstructionList");
+
+var theIngredients;
+var theInstructions;
+
 var url = "https://api.edamam.com/api/recipes/v2?type=public&q=Chicken&app_id=7446fad3&app_key=c948828625f59288cf410564484402b1";
 
 fetch(url)
@@ -11,19 +22,53 @@ fetch(url)
 .then(function (data){
     console.log(data);
     getLabels(data);
+    getIngredients(data);
+    getInstructions(data);
+    
+
+    
+    
     
 })
 
 
 function getLabels(thedata){
-    console.log(typeof(thedata.hits));
+   // console.log(typeof(thedata.hits));
    
    // console.log(thedata.hits[0].recipe.label);
     // for (var i = 0; i < 10; i++){
     //     console.log(thedata.hits[i].recipe.label)
     // }
+     var theList = [];
 
     for (const key in thedata.hits){
-        console.log(thedata.hits[key].recipe.label);
+       // console.log(thedata.hits[key].recipe.label);
+        theList.push(thedata.hits[key].recipe.label);
     }
+    console.log(theList);
+
+    var optionSelection;
+
+        
+        for (var i =0; i < theList.length; i++){
+           // console.log(theList[i]);
+            optionSelection += "<option>"+ theList[i] + "</option>";
+            
+        }
+
+        recipeSelect.innerHTML = optionSelection;
+
+
+}
+
+// get ingredients
+
+function getIngredients(thedata){
+    console.log(thedata.hits[0].recipe.ingredientLines)
+    console.log(typeof(thedata.hits[0].recipe.ingredientLines))
+
+}
+function getInstructions(thedata){
+    console.log(thedata.hits[0].recipe.instructionLines)
+
 }
